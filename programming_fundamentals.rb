7439b9d9890ec6f7b6fd16d3b0e52168ce0deb6c
Danny Chang <dannychachang@gmail.com>
2017-12-14 14:11:15 -0500

    Finish fundamentals with train array

---
+++ programming_fundamentals.rb
@@ -0,0 +1,67 @@
+trains = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+# 1. Save the direction of train 111 into a variable.
+
+dirtrain111 = trains.last[:direction]
+
+# 2. Save the frequency of train 80B into a variable.
+
+freqtrain80b = trains[5][:frequency_in_minutes]
+
+# 3. Save the direction of train 610 into a variable.
+
+dirtrain610 = trains.last[:direction]
+
+# 4. Create an empty array. Iterate through each train and add the name of the train into the array if it travels north.
+
+south = []
+
+trains.each do |train|
+  if train[:direction] == "south"
+    south << train[:train]
+  end
+end
+
+# 5. Do the same thing for trains that travel east.
+
+east = []
+
+trains.each do |train|
+  if train[:direction] == "east"
+    east << train[:train]
+  end
+end
+
+# 6. You probably just ended up rewriting some of the same code.
+# Move this repeated code into a method that accepts a direction and a list of trains as arguments,
+# and returns a list of just the trains that go in that direction.
+# Call this method once for north and once for east in order to DRY up your code.
+
+def direction(compass, trainlist)
+  nsew = []
+
+  trainlist.each do |train|
+    if train[:direction] == compass
+      nsew << train[:train]
+    end
+  end
+  return nsew
+end
+
+direction("north", trains)
+direction("east", trains)
+
+# 7. Pick one train and add another key/value pair for the first_departure_time.
+# For simplicity, assume the first train always leave on the hour.
+# You can represent this hour as an integer: 6 for 6:00am, 12 for noon, 13 for 1:00pm, etc.
+
+trains[0][:first_departure_time] = 6
